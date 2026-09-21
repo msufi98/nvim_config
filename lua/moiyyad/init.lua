304e97bfe98ce57eecb2a46541f28520ab5eb09b
Moiyyad <msufi98@gmail.com>
2026-09-20 21:56:04 -0500

    initial nvim config

---
+++ lua/moiyyad/init.lua
@@ -0,0 +1,30 @@
+require("moiyyad.set")
+require("moiyyad.remap")
+require("moiyyad.lazy_init")
+
+local augroup = vim.api.nvim_create_augroup
+local autocmd = vim.api.nvim_create_autocmd
+local yank_group = augroup('HighlightYank', {})
+
+function R(name)
+    require("plenary.reload").reload_module(name)
+end
+
+autocmd('TextYankPost', {
+    group = yank_group,
+    pattern = '*',
+    callback = function()
+        vim.hl.on_yank({
+            higroup = 'IncSearch',
+            timeout = 40,
+        })
+    end,
+})
+
+autocmd({"BufWritePre"}, {
+    group = augroup('MoiyyadTrimWhitespace', {}),
+    pattern = "*",
+    command = [[%s/\s\+$//e]],
+})
+
+vim.opt.isfname:append("@-@")
---
+++ lua/moiyyad/lazy/treesitter.lua
@@ -0,0 +1,20 @@
+return {
+    "nvim-treesitter/nvim-treesitter",
+    branch = "master",
+    build = ":TSUpdate",
+    config = function()
+        require("nvim-treesitter.configs").setup({
+            ensure_installed = {
+                "vimdoc", "javascript", "typescript", "c", "lua", "rust",
+                "jsdoc", "bash", "python", "json", "html", "css", "markdown",
+            },
+            sync_install = false,
+            auto_install = true,
+            indent = { enable = true },
+            highlight = {
+                enable = true,
+                additional_vim_regex_highlighting = { "markdown" },
+            },
+        })
+    end
+}
